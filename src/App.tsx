53b2b9042ed9c83d26b614bc513ce03e6e61973b
Vijay-patel-3333 <vp183190@gmail.com>
2022-11-18 17:41:36 -0500

    Routing added to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,26 +11,31 @@ import {
     Heading,
 } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
-import Shelf from "./components/Shelf";
-import { testBooks } from "./types/Book";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ShelfView from "./Shared/ShelfView";
 
 export const App = () => (
+    <Router>
+        
     <ChakraProvider theme={theme}>
         <Box textAlign="center" fontSize="xl">
             <Heading as="h1" mt="2rem">
                 Book Project
             </Heading>
             <Grid minH="100vh" p={3}>
-                <ColorModeSwitcher justifySelf="flex-end" />
-                <VStack spacing={3}>
-                    <Shelf id={1} shelfName="To Read" books={testBooks} />
-                    <Shelf id={2} shelfName="Reading" books={testBooks} />
-                    <Shelf id={3} shelfName="Finished" books={testBooks} />
-                    <Shelf id={4} shelfName="Want to Read" books={testBooks} />
-                    <Shelf id={5} shelfName="Favorites" books={testBooks} />
-                    <Shelf id={6} shelfName="Recommended" books={testBooks} />
-                </VStack>
+            <Routes>
+                <Route path="/" element={<>     
+                    <ColorModeSwitcher justifySelf="flex-end" />
+                    <ShelfView />
+                    </>}>
+                </Route>
+                
+            </Routes>
+                
+                
             </Grid>
         </Box>
     </ChakraProvider>
+    
+    </Router>
 );
---
+++ src/Shared/ShelfView.tsx
@@ -0,0 +1,16 @@
+import { VStack } from "@chakra-ui/react";
+import { testBooks } from "../types/Book";
+import Shelf from "../components/Shelf";
+
+export default function ShelfView() {
+    return (
+        <VStack spacing={3}>
+                    <Shelf id={1} shelfName="To Read" books={testBooks} />
+                    <Shelf id={2} shelfName="Reading" books={testBooks} />
+                    <Shelf id={3} shelfName="Finished" books={testBooks} />
+                    <Shelf id={4} shelfName="Want to Read" books={testBooks} />
+                    <Shelf id={5} shelfName="Favorites" books={testBooks} />
+                    <Shelf id={6} shelfName="Recommended" books={testBooks} />
+                </VStack>
+    );
+}
