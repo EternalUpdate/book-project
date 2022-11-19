83bb4218bfd7be2badd57559155663d2f6824158
Eugenie Tremblay <eugenie.tremblay@gmail.com>
2022-11-19 01:24:45 -0500

    Formatting changes to BookOverview page

--- src/components/BookOverview.tsx
+++ src/components/BookOverview.tsx
@@ -1,10 +1,10 @@
 import React, { ReactElement } from "react";
 import { Book, testBooks } from "../types/Book";
-import {Link, useLocation } from 'react-router-dom';
-import { Stack, Image, Heading, Box, Button, Flex } from "@chakra-ui/react";
+import { Link, useLocation } from "react-router-dom";
+import { Image, Heading, Text, Box, Button, Flex } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
 
-import './BookOverview.css';
+// import "./BookOverview.css";
 
 type Props = {
     id: number;
@@ -12,32 +12,58 @@ type Props = {
     books: Book[];
 };
 
-function BookOverview(){
-    const location = useLocation()
+function BookOverview() {
+    const location = useLocation();
     const book = location.state;
-    const navigate=useNavigate();
+    const navigate = useNavigate();
     const goBack = () => {
         navigate(-1);
-      };
-    
+    };
+
     return (
-        <div>
-        <h1 id="center">Book Details</h1>
-        <div className="img">
-        <img src={book.bookInfo.cover_url} alt="image"/>
-        </div>
-        <div className="detail">
-        <p className="txt">Title: {book.bookInfo.title}</p>
-        <p className="txt">Shelf: </p>
-        <p className="txt">Genre:{book.bookInfo.genre}</p>
-        <p className="txt">Author:{book.bookInfo.author}</p>
-        </div>
-        <button onClick={goBack} className='back-button'>
-        &larr; Go Back
-      </button>
-        </div>
+        <Flex direction="column" alignItems="center" p="1rem">
+            <Heading as="h1">Book Details</Heading>
+            <Flex direction="row" m="4rem">
+                <Image
+                    src={book.bookInfo.cover_url}
+                    maxHeight="sm"
+                    boxShadow="dark-lg"
+                    alt="image"
+                    className="img"
+                />
+                <Box
+                    className="detail"
+                    ml="4rem"
+                    p="2rem"
+                    // bgColor="gray.50"
+                    minWidth="2xs"
+                >
+                    <Heading as="h2" fontSize="3xl" mb="1rem">
+                        {book.bookInfo.title}
+                    </Heading>
+                    <Text>
+                        <b>Shelf:</b>
+                    </Text>
+                    <Text>
+                        <b>Genre(s):</b> {book.bookInfo.genre}
+                    </Text>
+                    <Text>
+                        <b>Author(s):</b> {book.bookInfo.author}
+                    </Text>
+                    <Text>
+                        <b>Summary:</b> {book.bookInfo.blurb}
+                    </Text>
+                </Box>
+            </Flex>
+            <Button
+                onClick={goBack}
+                className="back-button"
+                justifySelf="right"
+            >
+                &larr; Go Back
+            </Button>
+        </Flex>
     );
 }
 
-
-export default BookOverview
+export default BookOverview;
