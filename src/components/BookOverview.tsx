ac2f63ca3d640c624350ad3e4eacf02501a3f249
Vijay-patel-3333 <vp183190@gmail.com>
2022-11-28 16:55:14 -0500

    Moving book feature added

--- src/components/BookOverview.tsx
+++ src/components/BookOverview.tsx
@@ -4,6 +4,7 @@ import { shelfNames } from "../components/Shelf";
 import { Link, useLocation } from "react-router-dom";
 import { Image, Heading, Text, Box, Button, Flex } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+import {useState, useEffect} from "react";
 
 // import "./BookOverview.css";
 
@@ -13,13 +14,67 @@ type Props = {
     books: Book[];
 };
 
+ const shelfIDS: { [key: string]:Number } = {
+     "To Read":0,
+     "Reading":1,
+     "Finished":2,
+     "Did Not Finish":3,
+     "Favorites":4,
+     "Recommended":5,
+};
+
+
+
 function BookOverview() {
     const location = useLocation();
     const book = location.state;
     const navigate = useNavigate();
+    const [edit,setEdit]=useState(false);
+    const [currentShelf,setCurrentShelf]=useState(shelfNames[book.userBook.shelf_id])
+    const [currentBook,setCurrentBook]=useState(book)
+    const [currentUserBook,setCurrentUserBook]=useState(book.userBook)
+
+
+    const handleEdit = () => {
+        setEdit(true);
+    };
+
     const goBack = () => {
         navigate(-1);
     };
+
+
+    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        // do the rest here
+        console.log(event.target.value);
+        setCurrentShelf(event.target.value); 
+        
+         
+        console.log(shelfIDS[event.target.value]);
+        
+        setCurrentUserBook({...currentUserBook,shelf_id:shelfIDS[event.target.value]});
+        setCurrentBook([{...currentBook, userBook:currentUserBook } ])
+        console.log(currentBook)
+      }
+
+
+      useEffect(() => {
+        // POST request using fetch inside useEffect React hook
+        
+        fetch(`https://Localhost:8000/users/${book.userBook.user_id}/books/${book.isbn}`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json',
+                        'Access-Control-Allow-Origin': '*' },
+            body: JSON.stringify(currentBook)
+        })
+            .then(response => 
+                console.log(currentBook)
+                )
+                .catch((error) => console.log("error", error));
+    // empty dependency array means this effect will only run once (like componentDidMount in classes)
+    }, [currentShelf]);
+
+      
 
     return (
         <Flex direction="column" alignItems="center" p="1rem">
@@ -43,8 +98,21 @@ function BookOverview() {
                         {book.bookInfo.title}
                     </Heading>
                     <Text>
-                        <b>Shelf:</b> {shelfNames[book.userBook.shelf_id]}
+                        <b>Shelf:</b> 
+                    
+                    {edit ? 
+                    <select name="dog-names" id="dog-names" onChange={handleChange} value={currentShelf} defaultValue={currentShelf}>
+                        <option value="To Read">To Read</option>
+                        <option value="Reading">Reading</option>
+                        <option value="Finished">Finished</option>
+                        <option value="Did Not Finish">Did Not Finish</option>
+                        <option value="Favorites">Favorites</option>
+                        <option value="Recommended">Recommended</option>
+                    </select>: shelfNames[book.userBook.shelf_id]
+                    }
                     </Text>
+                    
+                    
                     <Text>
                         <b>Genre(s):</b> {book.bookInfo.genre}
                     </Text>
@@ -60,6 +128,10 @@ function BookOverview() {
                     <Text>
                         <b>Review:</b> {book.userBook.review}
                     </Text>
+                    <br/>
+                    <Button  onClick={handleEdit} colorScheme='teal' size='sm'>
+                        Edit
+                    </Button>
                 </Box>
             </Flex>
             <Button
